7a87f31f278cfb420d24a6d7367c04c603192a8b
Leomau <mauriciorosasg@gmail.com>
2023-09-05 11:54:47 -0600

    Avances en diferenciación automática

--- Apuntes/Diferenciación automática.jl
+++ Apuntes/Diferenciación automática.jl
@@ -1,12 +1,42 @@
-mutable struct Dual
-    funcion::Float64
-    derivada::Float64
+mutable struct Dual{T <: Real}
+    fun :: T
+    der :: T
 end
 
-# Definición de la suma:
-import Base.:+
-Base.:+(x::Dual,y::Dual) = Dual(x.funcion+y.funcion,x.derivada+y.derivada)
+function Dual(x::T,y::S) where {T<:Real, S<:Real}
+    return Dual(promote(x,y)...)
+end
 
-Dual(5.0,2.0) + Dual(1.0,1.0)
+function fun(x::Dual)
+    return x.fun
+end
 
+function der(x::Dual)
+    return x.der
+end
 
+function Dual(c::Real)
+    return Dual(c,0)
+end
+
+function dual(x)
+    return Dual(x,1)
+end
+
+import Base.:+, Base.:-, Base.:*, Base.:/
+
+function +(x::Dual,y::Dual)
+    Dual(x.fun+y.fun,x.der+y.der)
+end
+
+function -(x::Dual,y::Dual)
+    Dual(x.fun-y.fun,x.der-y.der)
+end
+
+function *(x::Dual,y::Dual)
+    Dual(x.fun*y.fun,x.fun*y.der+x.der*y.fun)
+end
+
+function /(x::Dual,y::Dual)
+    Dual(x.fun/y.fun,(y.fun*x.der-x.fun*y.der)/y.fun^2)
+end
